4ed2b836e054b914eb40914e0522c5ef03db1868
Salman Sharif <68875419+salmanameen@users.noreply.github.com>
2021-04-06 21:13:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const http=require("http");
-const fs=require("fs");
-const hostname="127.0.0.1";
-const port=3000;
-
-
-const home=fs.readFileSync("index.html");
+const http=require("http"); //  Import http Module 
+const fs=require("fs"); // Import fs Module 
+const hostname="127.0.0.1"; // Set local host
+const port=3000; // set port
+ 
+const home=fs.readFileSync("index.html"); 
 const about=fs.readFileSync("about.html");
 const services=fs.readFileSync("services.html");
 const contact=fs.readFileSync("contact.html");
+
 
 const server=http.createServer((req, res)=>{
 
@@ -44,7 +44,7 @@ const server=http.createServer((req, res)=>{
 
 })
 
-
+// listen the server
 server.listen(port, hostname , ()=>{
     console.log(`Server Running At: http://${hostname}:${port}/`)
 })
